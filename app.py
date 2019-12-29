3efdbe03b3e1783c48b7a0cf1767c7e6beee41a7
Ruoqiu Zhang <zhang_r@epita.fr>
2019-12-29 23:05:36 +0100

    [scrapper] Add linkedin scrapper.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from config import app
+from scrapper import LinkedinScrapper
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ scrapper.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+
+from config import app
+
+
+class LinkedinScrapper:
+    login_page = 'https://www.linkedin.com/uas/login-submit'
+    login_email = app.config['LINKEDIN_USER']
+    login_password = app.config['LINKEDIN_PASS']
+    session = None
+
+    def __init__(self):
+        payload = {
+            'session-key': self.login_email,
+            'session-password': self.login_password
+        }
+        s = requests.session()
+        s.post(self.login_page, data=payload)
+        self.session = s
+
+    def parse(self, url):
+        body = self.session.get(url).text
+        soup = BeautifulSoup(body, 'html.parser')
+        result = self.find_basic_info(soup)
+        return result
+
+    @staticmethod
+    def find_basic_info(soup):
+        result = {}
+        div = soup.find('div', {'class': 'topcard__content-left'})
+        title = div.find('h1', {'class': 'topcard__title'})
+        result['title'] = title.string
+        company = div.find('a', {'class': 'topcard__org-name-link topcard__flavor--black-link'})
+        result['company'] = company.string
+        location = div.find('span', {'class': 'topcard__flavor topcard__flavor--bullet'})
+        result['location'] = location.string
+        publish_date = div.find('span', {'class': 'topcard__flavor--metadata posted-time-ago__text'})
+        result['publish_date'] = publish_date.string
+        content = soup.find('div', {'class': 'description__text description__text--rich'})
+        result['content'] = content
+        return result
+
+
+if __name__ == '__main__':
+    html = 'https://www.linkedin.com/jobs/view/1621025764'
+    ls = LinkedinScrapper()
+    res = ls.parse(html)
+    print(res)
+
+
